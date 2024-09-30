eba25bfe92e611d57a739070bbeb9efd1bfed2cf
Reetu M Tambre <125602092+ReetuMT@users.noreply.github.com>
2024-09-30 11:49:07 +0530

    Add files via upload

---
+++ Python Topics/Tuples.py
@@ -0,0 +1,8 @@
+# A tuple is an immutable data type.element can not be changed, once defined a tuple cant be modified
+# tuple can be create using () paranthesis
+
+num=(1,2,4,52, 4)
+print(num)
+
+# 1. count: it will return the number of times the given values accurs in tuple
+print(num.count(4))  # 2
