659dfde1b523ec99c3e57674fd03a56cc4a79326
Reetu M Tambre <125602092+ReetuMT@users.noreply.github.com>
2024-10-01 16:28:35 +0530

    Add files via upload

---
+++ Python Topics/MathFunction.py
@@ -0,0 +1,25 @@
+import math
+
+# 1.Squt
+print("Square root ")
+a=math.sqrt(25)  # 5
+print(a)
+
+# 2.power
+print(math.pow(2,3)) # 8
+
+# 3. floor
+print(math.floor(4.7))  # 4
+print(math.floor(-2.6))  # -3
+
+# 4. ceil
+print(math.ceil(6.7))  # 7
+print(math.ceil(-6.4)) # -6
+
+
+# 5. pi 
+print(math.pi)  # 3.14
+
+
+
+
---
+++ Python Topics/Operator.py
@@ -0,0 +1,43 @@
+#Airthemetic 
+x=2
+y=10
+print(x+y) #12
+print(x-y) # -8
+print(x/y) # 0.2
+print(x*y) # 20
+print("----------------------------")
+#Assignemnt Operator
+a=3
+a=a+4
+print("A value is  " ,a) # 7
+a +=4
+print("a +=4 is " ,a) # 7+4 = 1
+
+print("----------------------------")
+#Relation Operator
+
+print(34 > 12)  #True
+print(21>= 34) # False
+print(19<124) # True
+print(3<=1) #False
+print(8==8)  #True
+print (3!=8) # True
+print("----------------------------")
+# Logical Operator
+
+a=2
+b=4
+
+print(a < 2 and b>3) # False
+print(a < 2 or b>3) # True
+print( not a) # False
+
+# Binary Operator
+print(bin(2)) # 0b10
+print(oct(7)) # 0o7
+print(hex(14)) # 0xe
+
+
+
+
+
